fc958862a4ba41f3d9f76201ab61004097dd2d47
pipa_ngry <pppipa2006@gmail.com>
2026-06-08 19:29:14 +0500

    Fix previous commit

--- src/boxed.rs
+++ src/boxed.rs
@@ -325,7 +325,7 @@ impl<'a, T> Box<'a, [MaybeUninit<T>]> {
     }
 }
 
-impl<'a, T: ?Sized> Deref for Box<'a, T> {
+impl<'a, T: ?Sized + 'a> Deref for Box<'a, T> {
     type Target = T;
 
     #[inline]
@@ -334,28 +334,28 @@ impl<'a, T: ?Sized> Deref for Box<'a, T> {
     }
 }
 
-impl<'a, T: ?Sized> DerefMut for Box<'a, T> {
+impl<'a, T: ?Sized + 'a> DerefMut for Box<'a, T> {
     #[inline]
     fn deref_mut(&mut self) -> &'a mut Self::Target {
         unsafe { self.ptr.as_mut() }
     }
 }
 
-impl<'a, T: ?Sized> AsRef<T> for Box<'a, T> {
+impl<'a, T: ?Sized + 'a> AsRef<T> for Box<'a, T> {
     #[inline]
     fn as_ref(&self) -> &'a T {
         self.deref()
     }
 }
 
-impl<'a, T: ?Sized> AsMut<T> for Box<'a, T> {
+impl<'a, T: ?Sized + 'a> AsMut<T> for Box<'a, T> {
     #[inline]
     fn as_mut(&mut self) -> &'a mut T {
         self.deref_mut()
     }
 }
 
-impl<'a, T: ?Sized> Borrow<T> for Box<'a, T> {
+impl<'a, T: ?Sized + 'a> Borrow<T> for Box<'a, T> {
     #[inline]
     fn borrow(&self) -> &'a T {
         self.deref()
